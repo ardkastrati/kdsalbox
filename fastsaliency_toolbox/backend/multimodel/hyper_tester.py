a58add7648eeb85ae4471aa4f3eb7766c75e0613
Yanick Zengaffinen <yanick.zengaffinen@gmail.com>
2022-05-03 10:52:02 +0200

    added KL

--- fastsaliency_toolbox/backend/multimodel/hyper_tester.py
+++ fastsaliency_toolbox/backend/multimodel/hyper_tester.py
@@ -1,13 +1,14 @@
 import os
 import numpy as np
 import torch
+from torch.nn import KLDivLoss
 from torch.utils.data import DataLoader
 
 import wandb
 
 from backend.utils import print_pretty_header
 from backend.datasets import TestDataManager
-from backend.metrics import NSS, CC, SIM
+from backend.metrics import NSS, CC, SIM, KL as KL_c
 from backend.image_processing import process
 from backend.parameters import ParameterMap
 
@@ -50,6 +51,8 @@ class HyperTester(object):
         self._data_offset = np.random.randint(0, (min_ds_len - imgs_per_task_test) // batch_size)
         self._dataloaders = {task:DataLoader(ds, batch_size=batch_size, shuffle=False, num_workers=4) for (task,ds) in zip(tasks,test_datasets)}
 
+        self.KL = KLDivLoss(reduce="batchmean")
+
         # sanity checks
         assert imgs_per_task_test <= min_ds_len
 
@@ -64,21 +67,24 @@ class HyperTester(object):
 
         if self._verbose: print(f"Testing {task}...")
 
-        all_names, all_loss, all_NSS, all_CC, all_SIM = [], [], [], [], []
+        all_names, all_loss, all_NSS, all_CC, all_SIM, all_KL, all_KL_c = [], [], [], [], [], [], []
         my_loss = torch.nn.BCELoss()
 
         data_iter = iter(dataloader)
         for i in range(self._data_offset): next(data_iter) # skip first few images
 
         for i in range(self._batches_per_task_test):
+            if i > 0: break
             (X, y, names) = next(data_iter)
             X = X.to(self._device)
             y = y.to(self._device)
 
             pred = model(task_id, X)
             losses = my_loss(pred, y)
-            detached_pred = pred.cpu().detach().numpy()
-            detached_y = y.cpu().detach().numpy()
+            detached_pred_t = pred.cpu().detach()
+            detached_y_t = y.cpu().detach()
+            detached_pred = detached_pred_t.numpy()
+            detached_y = detached_y_t.numpy()
 
             # Doing the postprocessing steps needed for the metrics (We might want to do this also for Task Evaluation stuff?)
             y = process(y, self._postprocess_parameter_map)
@@ -86,13 +92,17 @@ class HyperTester(object):
             NSSes = [NSS(map1, map2) for (map1, map2) in zip(detached_pred, detached_y)]
             CCes = [CC(map1, map2) for (map1, map2) in zip(detached_pred, detached_y)]
             SIMes = [SIM(map1, map2) for (map1, map2) in zip(detached_pred, detached_y)]
+            KLes = [self.KL(map1, map2) for (map1, map2) in zip(detached_pred_t, detached_y_t)]
+            KL_ces = [KL_c(map1, map2) for (map1, map2) in zip(detached_pred, detached_y)]
 
             with torch.no_grad():
+                all_names.extend(names)
+                all_loss.append(losses.item())
                 all_NSS.extend(NSSes)
                 all_CC.extend(CCes)
                 all_SIM.extend(SIMes)
-                all_loss.append(losses.item())
-                all_names.extend(names)
+                all_KL.extend(KLes)
+                all_KL_c.extend(KL_ces)
 
             if torch.cuda.is_available():
                 # Remove batch from gpu
@@ -103,14 +113,14 @@ class HyperTester(object):
             if i%100 == 0:
                 print(f"Batch {i}/{self._batches_per_task_test}: current accumulated loss {np.mean(all_loss)}", flush=True)
 
-            # save per image stats
-            if self._per_image_statistics:
-                stats_file = os.path.join(os.path.relpath(self._logging_dir, wandb.run.dir), "image_statistics", task).replace("\\", "/")
-                data = np.array([all_names, all_NSS, all_CC, all_SIM]).transpose().tolist()
-                table = wandb.Table(data=data, columns=["Image", "NSS", "CC", "SIM"])
-                wandb.run.log({stats_file:table})
+        # save per image stats
+        if self._per_image_statistics:
+            stats_file = os.path.join(os.path.relpath(self._logging_dir, wandb.run.dir), "image_statistics", task).replace("\\", "/")
+            data = np.array([all_names, all_NSS, all_CC, all_SIM, all_KL, all_KL_c]).transpose().tolist()
+            table = wandb.Table(data=data, columns=["Image", "NSS", "CC", "SIM", "KL", "KL custom"])
+            wandb.run.log({stats_file:table})
             
-        return np.mean(all_loss), np.nanmean(np.asarray(all_NSS)), np.nanmean(np.asarray(all_CC)), np.nanmean(np.asarray(all_SIM))
+        return np.mean(all_loss), np.nanmean(np.asarray(all_NSS)), np.nanmean(np.asarray(all_CC)), np.nanmean(np.asarray(all_SIM)), np.nanmean(np.asarray(all_KL)), np.nanmean(np.asarray(all_KL_c))
 
     def start_test(self):
         os.makedirs(self._logging_dir, exist_ok=True)
@@ -122,28 +132,32 @@ class HyperTester(object):
         model.eval()
 
         # foreach task
-        all_names, all_loss, all_NSS, all_CC, all_SIM = [], [], [], [], []
+        all_names, all_loss, all_NSS, all_CC, all_SIM, all_KL, all_KL_c = [], [], [], [], [], [], []
         for task,dataloader in self._dataloaders.items():
-            loss, NSS, CC, SIM = self.test_one(model, task, dataloader)
+            loss, NSS, CC, SIM, KL, KL_c = self.test_one(model, task, dataloader)
 
+            all_names.append(task)
+            all_loss.append(loss)
             all_NSS.append(NSS)
             all_CC.append(CC)
             all_SIM.append(SIM)
-            all_loss.append(loss)
-            all_names.append(task)
+            all_KL.append(KL)
+            all_KL_c.append(KL_c)
 
         # total
-        loss, NSS, CC, SIM = np.mean(all_loss), np.nanmean(np.asarray(all_NSS)), np.nanmean(np.asarray(all_CC)), np.nanmean(np.asarray(all_SIM))
+        loss, NSS, CC, SIM, KL, KL_c = np.mean(all_loss), np.nanmean(np.asarray(all_NSS)), np.nanmean(np.asarray(all_CC)), np.nanmean(np.asarray(all_SIM)), np.nanmean(np.asarray(all_KL)), np.nanmean(np.asarray(all_KL_c))
+        all_names.append("Average")
+        all_loss.append(loss)
         all_NSS.append(NSS)
         all_CC.append(CC)
         all_SIM.append(SIM)
-        all_loss.append(loss)
-        all_names.append("Average")
+        all_KL.append(KL)
+        all_KL_c.append(KL_c)
 
         # save test results
         stats_file = os.path.join(os.path.relpath(self._logging_dir, wandb.run.dir), "test_results").replace("\\", "/")
-        data = np.array([all_names, all_loss, all_NSS, all_CC, all_SIM]).transpose().tolist()
-        table = wandb.Table(data=data, columns=["Task", "Image", "NSS", "CC", "SIM"])
+        data = np.array([all_names, all_loss, all_NSS, all_CC, all_SIM, all_KL, all_KL_c]).transpose().tolist()
+        table = wandb.Table(data=data, columns=["Task", "Loss", "NSS", "CC", "SIM", "KL", "KL custom"])
         wandb.run.log({stats_file:table})
         
 
