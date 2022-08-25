77bb7cf024b9b6392b5ba4814f3feacb9e102cb0
Yanick Zengaffinen <yanick.zengaffinen@gmail.com>
2022-08-25 11:08:18 +0200

    eval optional

--- fastsaliency_toolbox/gmain.py
+++ fastsaliency_toolbox/gmain.py
@@ -95,28 +95,29 @@ def run_with_conf(conf, group=None):
         if "run" in conf.keys():
             stages.append(Runner(conf, "run", verbose=verbose))
         
-        # generate stats for each category
-        CAT = ['Action', 'Affective', 'Art', 'BlackWhite', 'Cartoon', 'Fractal', 'Indoor', 'Inverted', 'Jumbled', 'LineDrawing', 'LowResolution', 'Noisy', 'Object', 'OutdoorManMade', 'OutdoorNatural', 'Pattern', 'Random', 'Satelite', 'Sketch', 'Social']
-        UMSI = ['ads', 'infographics', 'mobile_uis', 'movie_posters', 'webpages']
-        
-        base_path = "/itet-stor/yanickz/net_scratch/data/dataset"
+        if "eval" in conf.keys():
+            # generate stats for each category
+            CAT = ['Action', 'Affective', 'Art', 'BlackWhite', 'Cartoon', 'Fractal', 'Indoor', 'Inverted', 'Jumbled', 'LineDrawing', 'LowResolution', 'Noisy', 'Object', 'OutdoorManMade', 'OutdoorNatural', 'Pattern', 'Random', 'Satelite', 'Sketch', 'Social']
+            UMSI = ['ads', 'infographics', 'mobile_uis', 'movie_posters', 'webpages']
+            
+            base_path = "/itet-stor/yanickz/net_scratch/data/dataset"
 
-        for cat in CAT:
-            base_path_sal = os.path.join(base_path, "saliency/CAT")
-            base_path_img = os.path.join(base_path, "DATASET/CAT")
-            stages.append(TesterFinal(conf, os.path.join(base_path_sal, cat), os.path.join(base_path_img, cat), f"table_cat_{cat}", verbose=verbose))
-        
-        for umsi in UMSI:
-            base_path_sal = os.path.join(base_path, "saliency/UMSI")
-            base_path_img = os.path.join(base_path, "DATASET/UMSI")
-            stages.append(TesterFinal(conf, os.path.join(base_path_sal, umsi), os.path.join(base_path_img, umsi), f"table_umsi_{umsi}", verbose=verbose))
+            for cat in CAT:
+                base_path_sal = os.path.join(base_path, "saliency/CAT")
+                base_path_img = os.path.join(base_path, "DATASET/CAT")
+                stages.append(TesterFinal(conf, os.path.join(base_path_sal, cat), os.path.join(base_path_img, cat), f"table_cat_{cat}", verbose=verbose))
+            
+            for umsi in UMSI:
+                base_path_sal = os.path.join(base_path, "saliency/UMSI")
+                base_path_img = os.path.join(base_path, "DATASET/UMSI")
+                stages.append(TesterFinal(conf, os.path.join(base_path_sal, umsi), os.path.join(base_path_img, umsi), f"table_umsi_{umsi}", verbose=verbose))
 
-        base_path_sal = os.path.join(base_path, "SALICON")
-        base_path_img = os.path.join(base_path, "SALICON/Images/val")
-        stages.append(TesterFinal(conf, base_path_sal, base_path_img, f"table_salicon", verbose=verbose))
+            base_path_sal = os.path.join(base_path, "SALICON")
+            base_path_img = os.path.join(base_path, "SALICON/Images/val")
+            stages.append(TesterFinal(conf, base_path_sal, base_path_img, f"table_salicon", verbose=verbose))
 
-        # generate tables from stats
-        stages.append(Tables(conf, "tables", verbose=verbose))
+            # generate tables from stats
+            stages.append(Tables(conf, "tables", verbose=verbose))
 
         mnet_fn = lambda : MNET(conf["model"]["mnet"])
         hnet_fn = lambda mnet : HNET(mnet.get_cw_param_shapes(), conf["model"]["hnet"])
